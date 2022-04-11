f092d588f3de53bd47542ebb8936bd019ba3b800
allan-gilber <allangilber@yahoo.com.br>
2022-04-11 17:33:03 -0300

    feat: created first error handling and included all build folder in gitignore.

--- src/utilities/mapAllLinks.ts
+++ src/utilities/mapAllLinks.ts
@@ -1,30 +1,36 @@
 import puppeteer from 'puppeteer';
 
 export const mapAllLinks = async (siteUrl: string)=>{
-	let urlToEvaluate = siteUrl;
-	const arrayOfLinks: string[]= [siteUrl];
+	try{
+		let urlToEvaluate = siteUrl;
+		const arrayOfLinks: string[]= [siteUrl];
 
-	while(urlToEvaluate){
-		const browser = await puppeteer.launch();
-		const webPage = await browser.newPage();
-		await webPage.goto(urlToEvaluate);
+		while(urlToEvaluate){
+			const browser = await puppeteer.launch({headless: false});
+			const webPage = await browser.newPage();
+			await webPage.goto(urlToEvaluate);
 
-		const getLink = await webPage.evaluate((): string => {
-			const link = document.querySelector('.next > a') as HTMLAnchorElement;
-			return link?.href ? link.href : '';
-		});
+			const getLink = await webPage.evaluate((): string => {
+				const link = document.querySelector('.next > a') as HTMLAnchorElement;
+				return link?.href ? link.href : '';
+			});
 
-		if(!getLink) {
-			console.log(`Finished link mapping. Found  pages ${arrayOfLinks.length} to scrap.`);
-			urlToEvaluate = '';
+			if(!getLink) {
+				console.log(`Finished link mapping. Found  pages ${arrayOfLinks.length} to scrap.`);
+				urlToEvaluate = '';
+				browser.close();
+				break;
+			}
+
+			urlToEvaluate = getLink;
+			arrayOfLinks.push(getLink);
+			console.log('Captured link: ', getLink);
 			browser.close();
-			break;
 		}
+		return arrayOfLinks;
 
-		urlToEvaluate = getLink;
-		arrayOfLinks.push(getLink);
-		console.log('Captured link: ', getLink);
-		browser.close();
+	}catch (error: any){
+		console.error('Something has gone wrong: ', error.message);
+		return;
 	}
-	return arrayOfLinks;
 };
