0643ad70a66276e4efc9ec1302ed203ef0454561
allan-gilber <allangilber@yahoo.com.br>
2022-04-11 22:24:19 -0300

    fix: removed some unused dependencys.

--- tests/mapAllLinks.test.ts
+++ tests/mapAllLinks.test.ts
@@ -1,26 +1,26 @@
 import {mapAllLinks} from'../src/utilities/mapAllLinks';
 
 
-describe('ValidateServerScraping',()=>{
+describe('Validate Web Scraping',()=>{
 	jest.setTimeout(60*1000);
 
-	test('Checklinkservermapping',async()=>{
+	test('Check link server mapping',async()=>{
 		expect.assertions(1);
 		try{
 			const result= await mapAllLinks('http:quotes.toscrape.com/');
 			expect(result?.length).toBeGreaterThan(1);
 		}catch(error){
-			console.log('Returnederror:',error);
-			expect(error).resolves.toContain('Somethinghasgonewrong:');
+			console.log('Returned error:',error);
+			expect(error).resolves.toContain('Something has gone wrong:');
 		}
 	});
-	test('Checklinkservermappingerrorforinvalid/unavaiblesite',async()=>{
+	test('Check link mapping error for invalid/unavaible site',async()=>{
 		try{
 			await mapAllLinks('http:a/');
 		}catch(error){
 			expect.assertions(1);
-			console.log('Returnederror:',error);
-			expect(error).resolves.toContain('Somethinghasgonewrong:');
+			console.log('Returned error:',error);
+			expect(error).resolves.toContain('Something has gone wrong:');
 		}
 		expect.assertions(0);
 	});
